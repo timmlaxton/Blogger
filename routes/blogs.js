0ca2dba8966b5924259918ef7b97880d8cf0e259
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-02-10 09:02:13 +0000

    can update and delte contact

--- routes/blogs.js
+++ routes/blogs.js
@@ -57,15 +57,51 @@ module.exports = router;
 // PUT api/blogs/:id
 // Update existing blog
 // Private
-router.put('/:id', (req, res) => {
-	res.send('Update current blog');
+router.put('/:id', auth, async (req, res) => {
+	const { title, image, post } = req.body;
+
+	const blogFields = {};
+	if (title) blogFields.title = title;
+	if (image) blogFields.image = image;
+	if (post) blogFields.post = post;
+
+	try {
+		let blog = await Blogs.findById(req.params.id);
+
+		if (!blog) return res.status(404).json({ msg: 'Blog not found' });
+
+		if (blog.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'You do not have the correct permission' });
+		}
+		blog = await Blogs.findByIdAndUpdate(req.params.id, { $set: blogFields }, { new: true });
+
+		res.json(blog);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
 });
+
 module.exports = router;
 
 // DELETE api/blogs/:id
 // Delete a blog
 // Private
-router.delete('/:id', (req, res) => {
-	res.send('Delete blog');
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		let blog = await Blogs.findById(req.params.id);
+
+		if (!blog) return res.status(404).json({ msg: 'Blog not found' });
+
+		if (blog.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'You do not have the correct permission' });
+		}
+
+		await Blogs.findByIdAndRemove(req.params.id);
+		res.json({ masg: 'Blog has been removed' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
 });
 module.exports = router;
